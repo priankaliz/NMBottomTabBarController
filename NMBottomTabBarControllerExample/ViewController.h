ac797e218da0991cb264b01f00968b4e18517374
Prianka Liz Kariat <priankaliz@gmail.com>
2014-12-05 17:46:51 +0530

    Initial Commit

---
+++ NMBottomTabBarControllerExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NMBottomTabBarControllerExample
+//
+//  Created by Prianka Liz Kariat on 05/12/14.
+//  Copyright (c) 2014 Prianka Liz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ NMBottomTabBarControllerExample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NMBottomTabBarControllerExample
+//
+//  Created by Prianka Liz Kariat on 05/12/14.
+//  Copyright (c) 2014 Prianka Liz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
